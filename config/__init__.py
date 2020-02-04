173e6910006b662abf77734aba9bc00c77a95540
rinkky <rinkky@live.com>
2020-02-04 23:56:52 +0800

    fix: yaml safe load

--- config/__init__.py
+++ config/__init__.py
@@ -5,5 +5,5 @@ def get_all_products():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     products_file = os.path.join(dir_path, 'products.yml')
     with open(products_file) as f:
-        product_list = yaml.load(f)
+        product_list = yaml.safe_load(f)
     return product_list
